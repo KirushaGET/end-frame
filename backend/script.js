16e9081b5287de0da98ef157ab9a662e1e479054
KarenGrigoryan1999 <41522488+KarenGrigoryan1999@users.noreply.github.com>
2021-08-21 21:55:05 +0300

    cors was added

--- backend/script.js
+++ backend/script.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const {router} = require("./routes/index")
 const sequelize = require("./db")
+const cors = require("cors")
 
 const app = express()
+
+app.use(cors({
+    credentials:true,
+    origin:"http://localhost:3000"
+}))
 
 app.use(express.json())
 app.use('/api',router)
